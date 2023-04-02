bc0c8583a92f11a017c64a8f6be1684d8512043e
coskuncelik <coskuncelik@gmail.com>
2023-04-02 11:29:33 +0000

    input file create script added

---
+++ assignment1/create_input_files.py
@@ -0,0 +1,16 @@
+import random
+
+number_of_files = 10
+
+
+for ii in range(number_of_files):
+    filename = "input"+str(ii)+".txt"
+
+    f = open(filename, 'w')
+
+    nums = random.randint(5, 20)
+    f.write("%d \n" % (nums))
+
+    for num in range(nums):
+        n = random.randint(1,100)
+        f.write("%d " % (n))

5ee89bc8d7adb3b27af7fc83af0b6f6199f8d06c
Yavuz Ozturk <mustafayavuzozturk@gmail.com>
2023-05-13 22:31:17 +0000

    .

--- ass2/main.c
+++ ass2/main.c
@@ -5,15 +5,19 @@
 #define m 4
 #define s 1
 
-void doRowShift(int* pMat) {
+void doRowShift(int* Mat[m][m]) {
 	
-	int firstRow[m]
+	int sRow[m], i, j;
 
-	for (int i=0; i=m-1; i++) {
-		if (i==0)
-			firstRow[i] = Mat[i,j];
-		for (int j=0; j=(m-1); j++)
-			Mat[i][j] = Mat[(i-s)%m][j];
+	for (i=m-1; i=0; i--) {
+		if (i==(s-1))
+			for (j=0; j=(m-1); j++)
+				sRow[j] = Mat[i,j];
+	}
+	for (i=0; i=m-1; i++) {
+		for (j=0; j=(m-1); j++) {
+			Mat[i][j] = Mat[(i+s)%m][j];
+		}
 	}
 }
 
@@ -25,7 +29,7 @@ int main(int argc, char* argv[])
 		{9,10,11,12},
 		{13,14,15,16},
 	};
-	doRowShift(&Mat[0][0]);
+	doRowShift(&Mat);
 	for (int i=0; i<m; i++) {
 		for (int j=0; j<m; j++)
     		printf("%d ", Mat[i][j]);

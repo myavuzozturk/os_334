2cc297e5f01e422251082f721915361f1ad74c55
Yavuz Ozturk <mustafayavuzozturk@gmail.com>
2023-05-14 11:29:00 +0000

    .

--- ass2/main.c
+++ ass2/main.c
@@ -5,21 +5,37 @@
 #define m 4
 #define s 1
 
-void doRowShift(int Mat[m][m]) {
-	
+void doRowShift(int* pMat) 
+{
 	int firstRow[m], i, j, k;
 	// printf("%d \n",Mat[1][1]);
-	// for (i=1; i<=s; i++) {
+	for (k=1; k<=s; k++) {
 		for (j=0; j<m; j++)
 			firstRow[j] = Mat[0][j];
 		for (j=0; j<m; j++) {
-			for (k=0; k<(m-1); k++)
-				Mat[k][j] = Mat[k+1][j];
+			for (i=0; i<(m-1); i++)
+				Mat[i][j] = Mat[i+1][j];
 		}
 		for (j=0; j<m; j++)
 			Mat[m-1][j] = firstRow[j];
-	// }
+	}
 }
+
+// void doColShift(int pMat) 
+// {
+// 	int lastCol[m], i, j, k;
+// 	// printf("%d \n",Mat[1][1]);
+// 	for (k=1; k<=s; k++) {
+// 		for (i=0; i<m; i++)
+// 			lastCol[i] = Mat[m-1][i];
+// 		for (i=(m-1); i>=0; i--) {
+// 			for (j=0; j<(m-1); j++)
+// 				Mat[i][j] = Mat[i][j+1];
+// 		}
+// 		for (i=0; i<m; i++)
+// 			Mat[i][m-1] = lastCol[i];
+// 	}
+// }
 
 int main(int argc, char* argv[]) 
 {
@@ -29,7 +45,9 @@ int main(int argc, char* argv[])
 		{9,10,11,12},
 		{13,14,15,16},
 	};
-	doRowShift(Mat);
+	int* pMat = &Mat[0][0];
+	doRowShift(pMat);
+	// doColShift(Mat);
 	for (int i=0; i<m; i++) {
 		for (int j=0; j<m; j++)
     		printf("%d ", Mat[i][j]);
